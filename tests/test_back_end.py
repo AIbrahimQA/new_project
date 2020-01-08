2951685a6b944707565c1f8fdead2fd9a8fbae99
AIbrahimQA <ahmed.ibrahim@academytrainee.com>
2020-01-08 19:28:29 +0000

    adding tests to services

--- tests/test_back_end.py
+++
@@ -1,6 +0,0 @@
-import unittest
-from flask import abort, url_for
-from flask_testing import TestCase
-from os import getenv
-from application import app, db
-from application.models import Passwords
